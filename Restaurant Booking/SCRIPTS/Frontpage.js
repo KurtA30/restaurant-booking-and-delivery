5fa9a0f57d1791e8f5c5d451389bd7ede857a2d7
KurtA30 <53459193+KurtA30@users.noreply.github.com>
2021-06-17 00:10:45 +0800

    TESTING LANGS TO

--- Restaurant Booking/SCRIPTS/Frontpage.js
+++ Restaurant Booking/SCRIPTS/Frontpage.js
@@ -1,5 +1,5 @@
 var imageloop = 1;
-var testVarAga = 2;
+var testingMuna = 2;
 document.getElementById('bg').src = "images/sectionbg1.jpg";
 document.getElementById('overlaysepbg').style.backgroundImage = "url('images/overlaybg1.jpg')";
 closeoverlay();
